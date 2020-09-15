69705c9efdf5fdb94f25ce74b426372dac64ebb9
Erik Kuhlmann <ekuhlmann23@gmail.com>
2020-09-15 11:49:36 -0600

    Create project structure for Server and Database projects.

---
+++ PRIME@UCR/Pages/Component.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Pages
+{
+    public partial class Component
+    {
+        public ComponentViewModel MyProperty { get; set; }
+    }
+}
