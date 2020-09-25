14b95cfdd30989cc7ab6ad9d7bdc1ca086a9f5ef
Isacc <isacc.zuniga@ucr.ac.cr>
2020-09-24 20:00:13 -0600

    Adding comments to merge it to DrimTeam

--- PRIME@UCR.Infrastructure/ApplicationDbContext.cs
+++ PRIME@UCR.Infrastructure/ApplicationDbContext.cs
@@ -7,5 +7,7 @@ namespace PRIME_UCR.Infrastructure
     public class ApplicationDbContext : DbContext
     {
         public DbSet<TestModel> TestModels { get; set; }
+        public DbSet<MultimediaContent> multimedia_content { get; set; }
+
     }
 }
