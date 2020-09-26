6bc0e4d1cba2d29e05bd29c3e31d863ea1e2326b
danisala03 <daniel.salazarmora@ucr.ac.cr>
2020-09-26 09:50:18 -0700

    Dropdown menu for Destination and Origin pages. Visual changes added. co-authored by: -Esteban Marin

--- PRIME@UCR/Components/Incidents/IncidentDetails/Tabs/OriginTab.razor.cs
+++ PRIME@UCR/Components/Incidents/IncidentDetails/Tabs/OriginTab.razor.cs
@@ -3,28 +3,53 @@ using System.Collections.Generic;
 
 namespace PRIME_UCR.Components.Incidents.IncidentDetails.Tabs
 {
+    //Enum with the options for selects
     enum OriginType
     {
         Household,
         International,
         MedicalCenter
     }
-    
+
+    enum CountryType {
+        CostaRica, 
+        UnitedStates,
+        Panama
+    }
+
     public partial class OriginTab
     {
-        private readonly List<(OriginType, string)> _dropdownValues = new List<(OriginType, string)>
+        // Selected options
+        private OriginType _selectedOriginType = OriginType.Household;
+
+        private CountryType _selectedCountryType = CountryType.CostaRica;
+
+        // Lists of options
+        private readonly List<(OriginType, string)> _dropdownValuesOrigin = new List<(OriginType, string)>
         {
             (OriginType.Household, "Domicilio"),
             (OriginType.International, "Internacional"),
             (OriginType.MedicalCenter, "Centro médico")
         };
 
-        private OriginType _selectedOriginType = OriginType.Household;
+        private readonly List<(CountryType, string)> _dropdownValuesCountries = new List<(CountryType, string)>
+        {
+            (CountryType.CostaRica, "Costa Rica"),
+            (CountryType.UnitedStates, "Estados Unidos"),
+            (CountryType.Panama, "Panamá")
+        };
         
-        void OnChange(OriginType originType)
+        //On click methods
+        void OnChangeOrigin(OriginType originType)
         {
             _selectedOriginType = originType;
         }
+
+        void OnChangeCountry(CountryType countryType)
+        {
+            _selectedCountryType = countryType;
+        }
+
     }
     
 }
--- PRIME@UCR/Components/Incidents/LocationPickers/MedicalCenterPicker.razor.cs
+++ PRIME@UCR/Components/Incidents/LocationPickers/MedicalCenterPicker.razor.cs
@@ -1,8 +1,28 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.Generic;
 
 namespace PRIME_UCR.Components.Incidents.LocationPickers
 {
     public partial class MedicalCenterPicker
     {
+        enum MedicalCenterType
+        {
+            Cima,
+            Mexico
+        }
+
+        //Selected type
+        private MedicalCenterType _selectedMedicalCenterType = MedicalCenterType.Cima;
+
+        private readonly List<(MedicalCenterType, string)> _dropdownValuesMedicalCenter = new List<(MedicalCenterType, string)>
+        {
+            (MedicalCenterType.Cima, "CIMA"),
+            (MedicalCenterType.Mexico, "México")
+        };
+
+        void OnChangeMedicalCenter(MedicalCenterType medicalType)
+        {
+            _selectedMedicalCenterType = medicalType;
+        }
+
     }
 }
