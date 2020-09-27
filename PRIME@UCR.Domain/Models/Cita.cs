b04063837e0b28c3df58058c52bd13a83b722424
Isacc <isacc.zuniga@ucr.ac.cr>
2020-09-27 00:22:19 -0600

    building modelts and relationships.

---
+++ PRIME@UCR.Domain/Models/Cita.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRIME_UCR.Domain.Models
+{
+    public class Cita
+    {
+        public string IDCita { get; set; }
+
+        public DateTime FechaHoraCreacion { get; set; }
+
+        public DateTime FechaHoraEstimada { get; set; }
+
+        public List<Acciones> Acciones { get; set; }
+
+
+        //PENDIENTES. 
+        //public List<CitaMedica>
+
+        //public List<Metricas>
+
+    }
+}
--- PRIME@UCR.Domain/Models/MultimediaContent.cs
+++ PRIME@UCR.Domain/Models/MultimediaContent.cs
@@ -20,5 +20,10 @@ namespace PRIME_UCR.Domain.Models
         public string Tipo { get; set; }
 
         public string ID_accion { get; set; }
+
+        //a relationship will be created when there is a navigation property discovered on a type. 
+        public Acciones Acciones { get; set; }
+
+        //public string ID_listaDeChequeo { get; set; } Este esta pendiente. 
     }
 }
