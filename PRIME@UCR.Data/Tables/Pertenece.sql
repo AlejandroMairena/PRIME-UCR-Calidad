79f355733aa8e44a2177aa880b38dd49a25c4966
fezequielmorales <fezequielmorales@hotmail.com>
2020-09-26 20:39:13 -0600

    Initial database setup

---
+++ PRIME@UCR.Data/Tables/Pertenece.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Pertenece]
+(
+	CédulaPersona		nvarchar(12)		NOT NULL,
+	CorreoUsuario		nvarchar(40)		NOT NULL,
+	NombrePerfil		nvarchar(40)		NOT NULL,
+	primary key (CédulaPersona, CorreoUsuario, NombrePerfil),
+	foreign key (CédulaPersona, CorreoUsuario)
+		references Usuario(CédulaPersona, Correo),
+	foreign key (NombrePerfil)
+		references Perfil(NombrePerfil)
+);
---
+++ PRIME@UCR.Data/Tables/Usuario.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	CédulaPersona		nvarchar(12)		NOT NULL,
+	Correo				nvarchar(40)		NOT NULL,
+	Contraseña			nvarchar(20)		NOT NULL,	
+	primary key(Correo),
+	foreign key(CédulaPersona)
+		references Persona(Cédula)
+);
