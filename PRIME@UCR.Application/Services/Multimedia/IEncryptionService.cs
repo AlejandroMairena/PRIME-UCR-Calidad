f975319bf7e3d7bb3c4b83897854780cc7f47988
Adrian Cordoba Soto <adrian.cordobasoto@ucr.ac.cr>
2020-09-28 17:26:48 -0600

    Added EncryptionService

---
+++ PRIME@UCR.Application/Services/Multimedia/IEncryptionService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRIME_UCR.Application.Services.Multimedia
+{
+    public interface IEncryptionService
+    {
+        public byte[] Key { get; set; }
+        public byte[] IV { get; set; }
+
+        public byte[] Encrypt(string plainText);
+
+        public string Decrypt(byte[] cipherText);
+
+        public string FileToString(string path);
+    }
+}
