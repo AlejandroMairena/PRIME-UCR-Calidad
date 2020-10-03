6279919813e69697de460c474c1cdd8c8b089a1e
Jose Viquez Ramirez <jose.viquezramirez@ucr.ac.cr>
2020-10-02 23:36:11 -0600

    Redundancy is removed from the User table in the database. Modifications are added to the sidebar based on the suggestions of the PO and stakeholders.

--- PRIME@UCR.Domain/Models/UserAdministration/Usuario.cs
+++ PRIME@UCR.Domain/Models/UserAdministration/Usuario.cs
@@ -8,7 +8,5 @@ namespace PRIME_UCR.Domain.Models.UserAdministration
     public class Usuario : IdentityUser 
     {
         public string CédulaPersona { get; set; }
-        public string Correo { get; set; }
-        public string Contraseña { get; set; }
     }
 }
