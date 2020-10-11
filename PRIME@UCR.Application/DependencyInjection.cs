3a7ba36c1483723dbaa88f55af76d1746d124e2c
Luis Andres Sanchez Romero <luis.sanchezromero@ucr.ac.cr>
2020-10-10 23:00:57 -0600

    Creation of new component "PermissionsComponent"
    UserStory: PIG01IIC20-423     ATE - Visualizacion y modificación de los permisos
    
    Changes:
    - Creation of PermissionsComponent
    - Creation of interface IPermissionsService
    - Creation of implementation PermissionsService
    - Permissions list is shown in UserAdministration/Profiles

--- PRIME@UCR.Application/DependencyInjection.cs
+++ PRIME@UCR.Application/DependencyInjection.cs
@@ -7,6 +7,8 @@ using PRIME_UCR.Application.Services;
 using PRIME_UCR.Application.Services.Incidents;
 using PRIME_UCR.Application.Services.Multimedia;
 using PRIME_UCR.Application.Implementations.Multimedia;
+using PRIME_UCR.Application.Implementations.UserAdministration;
+using PRIME_UCR.Application.Services.UserAdministration;
 
 namespace PRIME_UCR.Application
 {
@@ -15,7 +17,7 @@ namespace PRIME_UCR.Application
         public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
         {
             // services
-
+            services.AddTransient<IPermissionsService, PermissionsService>();
             services.AddTransient<ICheckListService, CheckListService>();
             services.AddTransient<IIncidentService, IncidentService>();
             services.AddTransient<ILocationService, LocationService>();
---
+++ PRIME@UCR/Components/UserAdministration/PermissionsComponent.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.Services.UserAdministration;
+using PRIME_UCR.Domain.Models.UserAdministration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.UserAdministration
+{
+    public partial class PermissionsComponent
+    {
+        
+        [Inject]
+        public IPermissionsService permissionsService { get; set; }
+
+        public List<Permiso> ListPermissions { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ListPermissions = (await permissionsService.GetPermisos()).ToList();
+        }
+
+    }
+}
