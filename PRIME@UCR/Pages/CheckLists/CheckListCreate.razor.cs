fb98e15bd5c21e099bdb6d78a091bd2739fd54d8
tefii2031 <stephanie.leitonramirez@ucr.ac.cr>
2020-10-12 14:45:04 -0600

    LG/Steph  New page ChecklistCreate and design changes  in SingleChecklist and checklist Page
    History:
    PIG01IIC20-70:in progres
    PIG01IIC20-309:in progres
    
    Change list:
    Start standar  design
    New page ( for later do Form)  instead of modal popup component
    
    By: LG/Steph

---
+++ PRIME@UCR/Pages/CheckLists/CheckListCreate.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.Services;
+using PRIME_UCR.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PRIME_UCR.Application.Services.CheckLists;
+using PRIME_UCR.Components.CheckLists;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq;
+
+
+namespace PRIME_UCR.Pages.CheckLists
+{
+    public class CheckListCreateBase : ComponentBase
+    {
+        [CascadingParameter(Name = "lists")]
+        protected IEnumerable<CheckList> lists { get; set; }
+
+        [Inject] protected ICheckListService MyService { get; set; }
+
+        private const string CreateUrl = "/checklist/create";
+        private string beforeUrl = "/checklist"; //mejorar diseño de interfaz
+    }
+}
