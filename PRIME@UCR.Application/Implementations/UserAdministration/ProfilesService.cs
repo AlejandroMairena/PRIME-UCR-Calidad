e87182b1d312f3e5512b6e5aca790d37fa07221c
fezequielmorales <fezequielmorales@hotmail.com>
2020-10-11 19:15:09 -0600

    Creation of new component "ProfilesComponent"
    UserStory: PIG01IIC20-411 ATE- Visualización y modificación de los perfiles
    
    Changes:
    - Creation of ProfilesComponent
    - Creation of interface IProfileService
    - Creation of implementation ProfilesService
    - Profile list is shown in UserAdministration/Profile

---
+++ PRIME@UCR.Application/Implementations/UserAdministration/ProfilesService.cs
@@ -0,0 +1,25 @@
+﻿using PRIME_UCR.Application.Repositories.UserAdministration;
+using PRIME_UCR.Application.Services.UserAdministration;
+using PRIME_UCR.Domain.Models.UserAdministration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Application.Implementations.UserAdministration
+{
+    public class ProfilesService : IProfilesService
+    {
+        private readonly IPerfilRepository _profilesRepository;
+
+        public ProfilesService(IPerfilRepository profileRepository)
+        {
+            _profilesRepository = profileRepository;
+        }
+
+        public async Task<IEnumerable<Perfil>> GetPerfiles()
+        {
+            return await _profilesRepository.GetAllAsync();
+        }
+    }
+}
---
+++ PRIME@UCR/Components/UserAdministration/ProfilesComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.Services.UserAdministration;
+using PRIME_UCR.Domain.Models.UserAdministration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.UserAdministration
+{
+    public partial class ProfilesComponent
+    {
+        [Inject]
+        public IProfilesService profilesService { get; set; }
+
+        public List<Perfil> ListProfiles { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ListProfiles = (await profilesService.GetPerfiles()).ToList();
+        }
+    }
+}
