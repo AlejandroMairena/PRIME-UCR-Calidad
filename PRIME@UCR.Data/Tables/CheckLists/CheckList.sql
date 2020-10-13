ded3f0b2b4fa21f2d212b125465db571b8aafba9
Pablo_Gamboa <juan.gamboavargas@ucr.ac.cr>
2020-10-12 20:30:38 -0600

    First draft of Edit CheckList User Story: PIG01IIC20-72 Editar listas de chequeo Tasks: PIG01IIC20-394 PIG01IIC20-395 Changes: - Added a function to CheckList service to update the database. - Added the option to edit each attribute in the SingleCheckListPage. - Fixing typing errors. - Extended the capacity of characters in the Checklist table.

--- PRIME@UCR.Data/Tables/CheckLists/CheckList.sql
+++ PRIME@UCR.Data/Tables/CheckLists/CheckList.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[CheckList](
 	[Id]			INT IDENTITY (1, 1) NOT NULL,
-	[Nombre]		NVARCHAR (50)		NOT NULL,
+	[Nombre]		NVARCHAR (100)		NOT NULL,
 	[Tipo]			NVARCHAR (20)		NOT NULL,
-	[Descripcion]	NVARCHAR (100)		NULL,
+	[Descripcion]	NVARCHAR (200)		NULL,
 	[Orden]			INT					NOT NULL,
 	[NombreImagen]	NVARCHAR (500)		DEFAULT 'defaultCheckList.png',
 	PRIMARY KEY CLUSTERED ([Id] ASC)
