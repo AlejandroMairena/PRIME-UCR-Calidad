69344a4b74689e46a13ef3b1c9db4416989d1b3d
Erick Rojas Zuñiga <b66310@ecci.ucr.ac.cr>
2020-10-13 10:21:07 -0600

    Creating the Item Table: PIG01IIC20-122 LG - Agregar Ítem a lista de chequeo TASK: PIG01IIC20-348  LG - Agregar tabla a BD. Creation of the Item table with all its attributes
    Alejandro Mairena
    Kevin Obando
    Erick Rojas

--- PRIME@UCR.Data/Tables/CheckLists/CheckList.sql
+++ PRIME@UCR.Data/Tables/CheckLists/CheckList.sql
@@ -4,6 +4,6 @@
 	[Tipo]			NVARCHAR (20)		NOT NULL,
 	[Descripcion]	NVARCHAR (200)		NULL,
 	[Orden]			INT					NOT NULL,
-	[NombreImagen]	NVARCHAR (500)		DEFAULT 'defaultCheckList.png',
+	[NombreImagen]	NVARCHAR (MAX)		DEFAULT 'defaultCheckList.png',
 	PRIMARY KEY CLUSTERED ([Id] ASC)
 )
---
+++ PRIME@UCR.Data/Tables/CheckLists/Item.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Item]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nombre ] NVARCHAR(150) NOT NULL, 
+    [ImagenDescriptiva] NVARCHAR(MAX) NOT NULL DEFAULT 'defaultCheckList.png', 
+    [Descripcion ] NVARCHAR(150) NULL , 
+    [Orden ] INT NOT NULL, 
+    [IDSuperItem] INT NOT NULL, 
+    [IDLista] INT NOT NULL, 
+    Foreign key (IDSuperItem) References Item (Id), 
+    Foreign key (IDLista) References CheckList (Id)	On Delete Cascade
+)
