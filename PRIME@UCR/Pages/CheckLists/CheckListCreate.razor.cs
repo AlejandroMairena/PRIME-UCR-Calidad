218a07276b68ebeb29980cd7b5f85ea71fc09190
tefii2031 <stephanie.leitonramirez@ucr.ac.cr>
2020-10-13 10:29:59 -0600

    LG/Steph  First  draft of checklist  create from page
    History:
    PIG01IIC20-70 Create checklist :in progres
    
    Change list:
    - Add in Checklist Create.razor.cs functionality to create a checklist correctly
    - AS-4
    By: LG/Steph  & LG/JuanPablo

--- PRIME@UCR/Pages/CheckLists/CheckListCreate.razor.cs
+++ PRIME@UCR/Pages/CheckLists/CheckListCreate.razor.cs
@@ -13,12 +13,62 @@ namespace PRIME_UCR.Pages.CheckLists
 {
     public class CheckListCreateBase : ComponentBase
     {
-        [CascadingParameter(Name = "lists")]
+   
         protected IEnumerable<CheckList> lists { get; set; }
 
         [Inject] protected ICheckListService MyService { get; set; }
 
+        [Inject]  public NavigationManager NavManager { get; set; }
+
         private const string CreateUrl = "/checklist/create";
         private string beforeUrl = "/checklist"; //mejorar diseño de interfaz
+        private string afterUrl = "";
+
+        protected CheckList checkList = new CheckList();
+
+        protected bool formInvalid = true;
+        protected EditContext editContext;
+
+        protected async Task RefreshModels()
+        {
+            lists = await MyService.GetAll();
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            editContext = new EditContext(checkList);
+            editContext.OnFieldChanged += HandleFieldChanged;
+            await RefreshModels();
+            checkList.Orden = lists.Count() + 1;
+        }
+
+        protected void HandleFieldChanged(object sender, FieldChangedEventArgs e)
+        {
+            if (checkList.Nombre != null && checkList.Tipo != null)
+            {
+                formInvalid = !editContext.Validate();
+            }
+            StateHasChanged();
+        }
+
+
+        public void Dispose()
+        {
+            editContext.OnFieldChanged -= HandleFieldChanged;
+        }
+
+        protected async Task AddCheckList(CheckList tempList)
+        {
+            await MyService.InsertCheckList(tempList);
+            await RefreshModels();
+        }
+
+
+        protected async Task HandleValidSubmit()
+        {
+            await AddCheckList(checkList);
+            afterUrl = "/checklist/" + checkList.Id;
+            NavManager.NavigateTo(afterUrl); // to do: agregar a pagina anterior
+        }
+
     }
 }
