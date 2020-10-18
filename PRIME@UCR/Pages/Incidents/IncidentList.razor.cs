8b2e4ff3b9fe0dea713c2d09e29f87aba7558fb8
Esteban Marín <jose.marinmasis@ucr.ac.cr>
2020-10-17 23:02:23 -0600

    Subject: Updated empty incident list from HTML to blazortable's display, with mock data. StoryID: PIG01IIC20-280 DV - Listado de incidentes general Story Status: In-Progress Changes: PRIME@UCR\Pages\Incidents\IncidentList.razor

--- PRIME@UCR/Pages/Incidents/IncidentList.razor.cs
+++ PRIME@UCR/Pages/Incidents/IncidentList.razor.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using PRIME_UCR.Application.Dtos.Incidents;
+using PRIME_UCR.Components.Incidents.IncidentDetails.Tabs;
+using PRIME_UCR.Domain.Models;
 
 namespace PRIME_UCR.Pages.Incidents
 {
     public partial class IncidentList
     {
+        private bool exists = false;
+
+        private SampleData[] data;
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(250);
+            data = Array.Empty<SampleData>();
+        }
+
+        public class SampleData
+        {
+            public int? id { get; set; }
+            public string full_name { get; set; }
+        }
     }
+
 }
+
 
