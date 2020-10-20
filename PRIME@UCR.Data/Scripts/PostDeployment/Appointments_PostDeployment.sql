62d5ee2c78c8f5881f6473e73d366e88621eccd6
Erik Kuhlmann <ekuhlmann23@gmail.com>
2020-10-20 12:03:11 -0600

    Add appointment tables to database.
    Story: PIG01IIC20-385
    SubTask: PIG01IIC20-481

---
+++ PRIME@UCR.Data/Scripts/PostDeployment/Appointments_PostDeployment.sql
@@ -0,0 +1,21 @@
+﻿DELETE FROM Cita
+DELETE FROM MetricasCitaMedica
+DELETE FROM MetricasIncidentes
+DELETE FROM Metricas
+DELETE FROM Accion
+DELETE FROM TipoAccion
+
+DBCC CHECKIDENT ('Cita', RESEED, 0)
+DBCC CHECKIDENT ('Metricas', RESEED, 0)
+DBCC CHECKIDENT ('Accion', RESEED, 0)
+
+INSERT INTO TipoAccion (Nombre, EsDeCitaMedica, EsDeIncidente)
+VALUES
+	('Complicación en traslado', 0, 1),
+	('Condición de colecta de paciente', 0, 1),
+	('Condición de entrega de paciente', 0, 1),
+	('Síntomas', 1, 1),
+	('Examen', 1, 0),
+	('Diagnóstico', 1, 0),
+	('Recetas médicas', 1, 0),
+	('Referencia a especialista', 1, 0)
---
+++ PRIME@UCR.Data/Tables/Appointments/Accion.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Accion]
+(
+	[Id] INT IDENTITY(1, 1) NOT NULL,
+	[CitaId] INT NOT NULL,
+	[NombreAccion] VARCHAR(50) NOT NULL,
+	[Descripcion] VARCHAR(500) NULL,
+	PRIMARY KEY (Id, CitaId),
+	FOREIGN KEY (CitaId)
+		REFERENCES Cita(Id),
+	FOREIGN KEY (NombreAccion)
+		REFERENCES TipoAccion(Nombre)
+)
