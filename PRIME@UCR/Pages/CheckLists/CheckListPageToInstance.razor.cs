794a3f262d9472ede7339c729019034e7f29bd6e
tefii2031 <stephanie.leitonramirez@ucr.ac.cr>
2020-10-20 01:29:22 -0600

    LG/Steph Changes in design: modify checklist and create item and sub item
    History:
    PIG01IIC20-72  Modify checklist: in progress
    PIG01IIC20-114 Add subitem to checklist
    PIG01IIC20-122 Add item to checklist
    PIG01IIC20-127  Assign checklist to incidents
    Task:
    PIG01IIC20-352 user interface code
    PIG01IIC20-469 create item page
    PIG01IIC20-395 user interface
    PIG01IIC20-424 interface realization: started
    
    Change list:
    - Change in the button for create a new item
    - Change in the button for create a new subitem
    - Design changes around the definition of a checklist and modification
    - Strat to design and implementation interaction with incidents
    
    By: LG/Steph

---
+++ PRIME@UCR/Pages/CheckLists/CheckListPageToInstance.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.Services;
+using PRIME_UCR.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PRIME_UCR.Application.Services.CheckLists;
+using PRIME_UCR.Components.CheckLists;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq;
+using PRIME_UCR.Domain.Models.CheckLists;
+
+namespace PRIME_UCR.Pages.CheckLists
+{
+    public class CheckListToInstanceBase : ComponentBase
+    {
+        protected IEnumerable<CheckList> lists { get; set; }
+
+        [Inject] protected ICheckListService MyService { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshModels();
+        }
+
+        protected async Task RefreshModels()
+        {
+            lists = await MyService.GetAll();
+        }
+    }
+}
