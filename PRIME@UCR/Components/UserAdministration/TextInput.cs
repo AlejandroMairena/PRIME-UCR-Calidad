95df65f1711185b02de95f2fa8e0cb9716b295a6
Elian Ortega <elian.mortega@gmail.com>
2020-10-25 16:23:51 -0600

    Added documentation for new methods User Story: PIG01IIC20-233 ATE - Autorización al módulo de administración de usuarios y perfiles
    Changes:
     - Added Documentation

--- PRIME@UCR/Components/UserAdministration/TextInput.cs
+++ PRIME@UCR/Components/UserAdministration/TextInput.cs
@@ -4,9 +4,14 @@ using Microsoft.AspNetCore.Components;
 
 namespace PRIME_UCR.Components.Controls
 {
+    //Implementation of GenericInput used in login form
     public class TextInput : GenericInput<String>
     {
         [Parameter] public string Type { get; set; }
+
+        //On Changed validation for the Input
+        //Only validates if inputs in not null or empty
+        // Other validations are made with the selected model on implementation
         protected override async Task OnChangeEvent(ChangeEventArgs e)
         {
             string result = (String)e.Value;
@@ -24,6 +29,7 @@ namespace PRIME_UCR.Components.Controls
             EditContext.NotifyFieldChanged(FieldIdentifier);
         }
 
+        //Override to change the type of input to be used
         protected override void OnInitialized()
         {
             _type = Type;
