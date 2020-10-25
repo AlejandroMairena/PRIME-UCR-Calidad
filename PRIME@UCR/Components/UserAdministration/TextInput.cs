c7efb58ac4e2d70333aea2a7ce85612081b642dc
Elian Ortega <elian.mortega@gmail.com>
2020-10-25 15:24:42 -0600

    Changed Inputs on login form to follow the same format as others in the app. Changed NavMenu disabled modules that aren't developed yet.
    UserStory:
    PIG01IIC20-363 ATE - Validación en la autenticación de usuarios
    
    Changes:
    - Changed Inputs on login form to follow the same format as others in the app.
    - Changed NavMenu disabled modules that aren't developed yet.
    - Disable change password link

---
+++ PRIME@UCR/Components/UserAdministration/TextInput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace PRIME_UCR.Components.Controls
+{
+    public class TextInput : GenericInput<String>
+    {
+        [Parameter] public string Type { get; set; }
+        protected override async Task OnChangeEvent(ChangeEventArgs e)
+        {
+            string result = (String)e.Value;
+            var valid = !String.IsNullOrEmpty(result);
+            if (valid)
+            {
+                Value = result;
+            }
+            else
+            {
+                Value = null;
+            }
+
+            await ValueChanged.InvokeAsync(Value);
+            EditContext.NotifyFieldChanged(FieldIdentifier);
+        }
+
+        protected override void OnInitialized()
+        {
+            _type = Type;
+        }
+    }
+}
