4b74699db25a179a61d9c339efd08d7dff5926df
Luis Andres Sanchez Romero <luis.sanchezromero@ucr.ac.cr>
2020-11-01 21:11:49 -0600

    Initial implementation of the Incidents vs Time chart
    User story     PIG01IIC20-567    ATE - Ver gráfico de incidentes en el tiempo
    
    Changes:
    - Added the IncidentsVsTimeChart component and its partial class.
    - Initial implementation of the IncidentsVstimeChart with random data.
    - Included the chart in the dashboard page.
    
    Developers:
    - Luis Sanchez Romero
    - Jose Viquez Ramirez
    - Fernando Morales

---
+++ PRIME@UCR/Components/Dashboard/IncidentsVsTimeChartComponent.razor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChartJs.Blazor.Charts;
+using ChartJs.Blazor.ChartJS.Common;
+using ChartJs.Blazor.ChartJS.Common.Properties;
+using ChartJs.Blazor.ChartJS.Common.Enums;
+using ChartJs.Blazor.ChartJS.LineChart;
+using ChartJs.Blazor.ChartJS.Common.Axes;
+using ChartJs.Blazor.ChartJS.Common.Axes.Ticks;
+using ChartJs.Blazor.ChartJS.Common.Handlers;
+using ChartJs.Blazor.Util;
+
+namespace PRIME_UCR.Components.Dashboard
+{
+    public partial class IncidentsVsTimeChartComponent
+    {
+        LineConfig _lineConfig;
+        ChartJsLineChart _lineChartJs;
+       
+        private LineDataset<Point> _incidentsPerDaySet;
+
+        protected override void OnInitialized()
+        {
+            _lineConfig = new LineConfig
+            {
+                Options = new LineOptions
+                {
+                    Responsive = true,
+                    Title = new OptionsTitle
+                    {
+                        Display = true,
+                        Text = "Incidentes en el tiempo"
+                    },
+                    Legend = new Legend
+                    {
+                        Display = false,
+                    },
+                    Tooltips = new Tooltips
+                    {
+                        Mode = InteractionMode.Nearest,
+                        Intersect = false
+                    },
+                    Scales = new Scales
+                    {
+                        xAxes = new List<CartesianAxis>
+                        {
+                            new LinearCartesianAxis
+                            {
+                                ScaleLabel = new ScaleLabel
+                                {
+                                    LabelString = "Tiempo"
+                                },
+                                GridLines = new GridLines
+                                {
+                                    Display = false
+                                },
+                                Ticks = new LinearCartesianTicks
+                                {
+                                    SuggestedMin = 0
+                                }
+                            }
+                        },
+                        yAxes = new List<CartesianAxis>
+                        {
+                            new LinearCartesianAxis
+                            {
+                                ScaleLabel = new ScaleLabel
+                                {
+                                    LabelString = "Incidentes"
+                                }
+                            }
+                        }
+                    },//finaliza Scales
+                    Hover = new LineOptionsHover
+                    {
+                        Intersect = true,
+                        Mode = InteractionMode.Y
+                    }//finaliza Hover
+
+                }//finaliza lineOptions
+            };//finaliza lineConfig
+            _incidentsPerDaySet = new LineDataset<Point>
+            {
+                BackgroundColor = ColorUtil.ColorString(255, 255, 255, 1.0),
+                BorderColor = ColorUtil.ColorString(0, 0, 255, 1.0),
+                Label = "Incidentes por día",
+                Fill = false,
+                PointBackgroundColor = ColorUtil.ColorString(0, 0, 255, 1.0),
+                BorderWidth = 1,
+                PointRadius = 3,
+                PointBorderWidth = 1,
+                SteppedLine = SteppedLine.False,
+            };
+
+            for(var i = 0; i < 10; i++)
+            {
+                _incidentsPerDaySet.Add(new Point(i*2, i+2));
+                //_tempPerDaySet.AddRange(weatherForecasts.Select(p => new Point(p.Date.Day, p.TemperatureF)));
+            }
+            _lineConfig.Data.Datasets.Add(_incidentsPerDaySet);
+
+             
+        }//finaliza metodo
+    }
+}
