0b76f219b6a2feaf448b8099f324c08a798a5e3e
jaszdev <javier.sandoval@ucr.ac.cr>
2020-11-02 17:27:42 -0600

    Added IJSRuntime service to call JS from MultimediaComponent and a JS Script to check if camara is available.
    PIG01IIC20-436: in-progress
    
    Changes:
    MultimediaComponent
    _Host
    Added:
    camara_test.js

---
+++ PRIME@UCR/wwwroot/js/camera_test.js
@@ -0,0 +1,13 @@
+﻿function hasGetUserMedia() {
+    var has_um = !!(navigator.mediaDevices &&
+        navigator.mediaDevices.getUserMedia);
+
+    if (has_um) {
+        // Good to go!
+        console.log('Good to go!');
+    } else {
+        //alert('getUserMedia() is not supported by your browser');
+        console.log('Not good to go!');
+    }
+    return has_um;
+}
