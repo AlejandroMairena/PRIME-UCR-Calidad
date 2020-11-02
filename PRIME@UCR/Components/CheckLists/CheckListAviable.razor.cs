0156162422d9aac93f3025d111d5b66a93ce3b34
tefii2031 <stephanie.leitonramirez@ucr.ac.cr>
2020-11-02 07:57:57 -0600

    LG/Steph Add new components  Interaction with Incident and all checklist
    History:
    PIG01IIC20-127 Asign a check list an incident
    
    Task:
    PIG01IIC20-424 Do interface: done
    PIG01IIC20-426 Do testings: in progress
    
    Change list:
    -New componentes( but not tested in this branch
    -Temporality changes in page "Single checklist in incident"
    
    By: LG/Steph

---
+++ PRIME@UCR/Components/CheckLists/CheckListAviable.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.Services;
+using PRIME_UCR.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PRIME_UCR.Application.Services.CheckLists;
+using PRIME_UCR.Components.CheckLists;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq;
+using PRIME_UCR.Domain.Models.CheckLists;
+
+namespace PRIME_UCR.Components.CheckLists
+{
+    /**
+    * This page displays every checklist and general data for each one
+    * */
+    public class CheckListAviableBase : ComponentBase
+    {
+        protected IEnumerable<CheckList> lists { get; set; }
+
+        [Inject] protected ICheckListService MyService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await RefreshModels();
+        }
+
+        /**
+         * Gets the list of checklists in the database
+         * */
+        protected async Task RefreshModels()
+        {
+            lists = await MyService.GetAll();
+        }
+    }
+}
