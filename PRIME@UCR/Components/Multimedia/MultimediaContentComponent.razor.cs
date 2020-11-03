823f57170b0dec76a9f6279660295fd0c1aab149
jaszdev <javier.sandoval@ucr.ac.cr>
2020-11-03 10:12:41 -0600

    Refactoring to divide HTML and C# code into two files. Changes: MultimediaContentComponent

---
+++ PRIME@UCR/Components/Multimedia/MultimediaContentComponent.razor.cs
@@ -0,0 +1,111 @@
+﻿using BlazorInputFile;
+using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.Multimedia
+{
+    public partial class MultimediaContentComponent
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public List<MultimediaContent> MultimediaContent { get; set; }
+        [Parameter]
+        public EventCallback<MultimediaContent> OnFileUpload { get; set; }
+
+        public List<string> validTypeFiles;
+        bool validFileType = true;
+
+        bool open = false;
+        string divDDClass = "dropdown";
+        string ddMenuClass = "dropdown-menu";
+        string invalidMessage = "";
+
+        // real time multimedia
+        bool showCamera = false;
+
+        protected override void OnInitialized()
+        {
+            validTypeFiles = new List<string>() { "pdf", "doc", "docx", "xls", "txt", "mp3", "jpg", "png", "mp4", "wmv", "avi", "text/plain" };
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await JS.InvokeAsync<bool>("hasGetUserMedia", null);
+        }
+
+        public void Open()
+        {
+            divDDClass = open ? "dropdown" : "dropdown show";
+            ddMenuClass = open ? "dropdown-menu" : "dropdown-menu show";
+
+            open = !open;
+        }
+
+        IFileListEntry file;
+
+        async Task HandleFileSelected(IFileListEntry[] files)
+        {
+            file = files.FirstOrDefault();
+
+            validFileType = ValidateFile(file, validTypeFiles);
+
+            if (!validFileType) return; // archivo invalido
+
+            string filePath = Path.Combine(file_service.FilePath, file.Name);
+            byte[] pathEncrypted = encrypt_service.Encrypt(filePath);
+            string pathEncryptedString = System.Text.Encoding.UTF8.GetString(pathEncrypted);
+            if (file == null) return;
+            MultimediaContent mcontent = new MultimediaContent
+            {
+                Nombre = file.Name,
+                Archivo = pathEncryptedString,
+                Descripcion = "",
+                Fecha_Hora = DateTime.Now,
+                Tipo = file.Type
+            };
+            mcontent = await multimedia_content_service.AddMultimediaContent(mcontent);
+            await file_service.StoreFile(file.Name, file.Data);
+            //encrypt_service.EncryptFile(file_service.FilePath,file.Name);
+            await OnFileUpload.InvokeAsync(mcontent);
+        }
+
+        bool ValidateFile(IFileListEntry file, List<string> validFileTypes)
+        {
+            if (file == null) return false;
+
+            string type = file.Type;
+
+            foreach (string validType in validTypeFiles)
+                if (type.Contains(validType) || type == validType) return true;
+
+            return false;
+        }
+
+        async Task OpenCamera()
+        {
+            showCamera = !showCamera;
+            if (showCamera)
+                await JS.InvokeAsync<bool>("openCamera", null);
+        }
+
+        string invalidType()
+        {
+            string datas = "El archivo seleccionado no se encuentra dentro de los archivos válidos. Por favor seleccione un archivo con las siguientes extensiones: ";
+            for (int i = 0; i < validTypeFiles.Count(); ++i)
+            {
+                datas += validTypeFiles[i];
+                if (i < (validTypeFiles.Count() - 1))
+                {
+                    datas += ",";
+                }
+            }
+            return datas;
+        }
+    }
+}
--- PRIME@UCR/wwwroot/js/camera_test.js
+++ PRIME@UCR/wwwroot/js/camera_test.js
@@ -11,3 +11,16 @@
     }
     return has_um;
 }
+
+function openCamera() {
+    const constraints = {
+        video: { width: { exact: 640 }, height: { exact: 480 } }
+    };
+
+    const video = document.querySelector('video');
+
+    navigator.mediaDevices.getUserMedia(constraints).
+        then((stream) => { video.srcObject = stream });
+
+    return true;
+}
