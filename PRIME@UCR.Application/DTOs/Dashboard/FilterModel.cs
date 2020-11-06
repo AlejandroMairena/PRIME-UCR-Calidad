7f6f0d1d0ae02cfddd532a47f4de71c83f24beb7
fezequielmorales <fezequielmorales@hotmail.com>
2020-11-06 10:14:52 -0600

    Implemented date filter and started the implementation of a DTO that is able to handle all of the filters in it
    UserStory:     PIG01IIC20-570
    ATE - Filtrar información por rango de fecha
    
    Changes:
    - Implemented DateFilter, which contains a start and final date, to function as a range of dates.
    - Started implementation of FilterModel DTO, which is gonna hold all of the filter related data
    
    Developers:
    - Fernando Morales

---
+++ PRIME@UCR.Application/DTOs/Dashboard/FilterModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using PRIME_UCR.Application.Dtos.Incidents;
+using PRIME_UCR.Domain.Models;
+using PRIME_UCR.Domain.Models.Incidents;
+using PRIME_UCR.Domain.Models.UserAdministration;
+
+namespace PRIME_UCR.Application.DTOs.Dashboard
+{
+    public class FilterModel
+    {
+        public FilterModel()
+        {
+        }
+
+        public DateTime? InitialDateFilter { get; set; }
+        public DateTime? FinalDateFilter { get; set; }
+
+        public CentroMedico MedicalCenterDestination { get; set; }
+
+        public HouseholdModel HouseholdOriginFilter { get; set; }
+        public InternationalModel InternationalOriginFilter { get; set; }
+        public MedicalCenterLocationModel MedicalCenterOriginFilter { get; set; }
+
+    }
+}
---
+++ PRIME@UCR/Components/Dashboard/Filters/DateFilter.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PRIME_UCR.Application.DTOs.Dashboard;
+
+namespace PRIME_UCR.Components.Dashboard.Filters
+{
+    public partial class DateFilter
+    {
+        public FilterModel Value { get; set; }
+
+        protected override async Task OnInitializedAsync() 
+        {
+            Value = new FilterModel();
+        }
+
+    }
+}
