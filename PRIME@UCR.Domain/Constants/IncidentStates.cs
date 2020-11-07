20ea6be720f2796a5427348f4ce23ecb885f4a1c
Esteban Marín <jose.marinmasis@ucr.ac.cr>
2020-11-06 18:34:43 -0600

    States Panel component created.
    Story: PIG01IIC20-569
    Subtask: PIG01IIC20-584 (in progress)
    
    Co-authored-by: Daniel Salazar
    WIP: Change style of states list, refactor state list view, create next state table

--- PRIME@UCR.Domain/Constants/IncidentStates.cs
+++ PRIME@UCR.Domain/Constants/IncidentStates.cs
@@ -9,5 +9,13 @@ namespace PRIME_UCR.Domain.Constants
         public static readonly Estado Created = new Estado {Nombre = "Creado"};
         public static readonly Estado Rejected = new Estado {Nombre = "Rechazado"};
         public static readonly Estado Approved = new Estado {Nombre = "Aceptado"};
+        public static readonly Estado Assigned = new Estado { Nombre = "Asignado" };
+        public static readonly Estado Preparing = new Estado { Nombre = "En preparación" };
+        public static readonly Estado InOriginRoute = new Estado { Nombre = "En ruta a origen" };
+        public static readonly Estado PatientInOrigin = new Estado { Nombre = "Paciente recolectado en origen" };
+        public static readonly Estado InRoute = new Estado { Nombre = "En traslado" };
+        public static readonly Estado Delivered = new Estado { Nombre = "Entregado" };
+        public static readonly Estado Reactivated = new Estado { Nombre = "Reactivado" };
+        public static readonly Estado Done = new Estado { Nombre = "Finalizado" };
     }
 }
---
+++ PRIME@UCR/Components/Incidents/IncidentDetails/Tabs/StatePanel.razor.cs
@@ -0,0 +1,37 @@
+﻿using MatBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using PRIME_UCR.Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.Incidents.IncidentDetails.Tabs
+{
+    public partial class StatePanel
+    {
+        public List<string> IncidentStatesList = new List<string> {
+            IncidentStates.InCreationProcess.Nombre,
+            IncidentStates.Created.Nombre,
+            IncidentStates.Rejected.Nombre,
+            IncidentStates.Approved.Nombre,
+            IncidentStates.Assigned.Nombre,
+            IncidentStates.Preparing.Nombre,
+            IncidentStates.InOriginRoute.Nombre,
+            IncidentStates.PatientInOrigin.Nombre,
+            IncidentStates.InRoute.Nombre,
+            IncidentStates.Delivered.Nombre,
+            IncidentStates.Reactivated.Nombre,
+            IncidentStates.Done.Nombre
+        };
+        MatButton Button2;
+        BaseMatMenu Menu2;
+
+        public void OnClick(MouseEventArgs e)
+        {
+            this.Menu2.OpenAsync(Button2.Ref);
+        }
+    }
+}
+
