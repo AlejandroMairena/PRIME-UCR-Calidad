6938d20c0093ef76e646f1cc29f087f1261a99fd
Ricardo Franco <richifran1301@gmail.com>
2020-11-07 18:43:18 -0600

    Worked on UI in permissions: view incident list, edit basic incident details, view basic incident details, create incident.
    Substasks ids: PIG01IIC20-585, PIG01IIC20-590, PIG01IIC20-593, PIG01IIC20-604
    
    Co-Programmers:
    Erik Kuhlmann
    Adrian Sibaja

---
+++ PRIME@UCR/Components/UserAdministration/PermissionAuthorizeView.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.DTOs.UserAdministration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.UserAdministration
+{
+    public partial class PermissionAuthorizeView
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public AuthorizationPermissions Permission { get; set; }
+    }
+}
