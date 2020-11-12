393f70f1f7804a2d3ec92ae6081bd0d9cd907287
fezequielmorales <fezequielmorales@hotmail.com>
2020-11-12 00:11:30 -0600

    Unit tests added to the StateService and PermiteService and added security to the StateService
    UserStory:
    PIG01IIC20-571
    ATE - Filtrar información por estado
    
    Changes:
    - Test added for methods of StateService
    - Test added for methods of PermiteService
    - Security added to the StateService
    - Added StateServicePermissions to implement security in the StateService
    
    Developers:
    - Fernando Morales

---
+++ PRIME@UCR.Application/Permissions/Incidents/StateServicePermissions.cs
@@ -0,0 +1,17 @@
+﻿using PRIME_UCR.Domain.Attributes;
+using PRIME_UCR.Domain.Constants;
+using PRIME_UCR.Domain.Models.Incidents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Application.Permissions.Incidents
+{
+    public abstract class StateServicePermissions
+    {
+        [RequirePermissions(new[] { AuthorizationPermissions.CanSeeIncidentsInfoOnDashboard })]
+        public abstract Task<IEnumerable<Estado>> GetIncidentCounterAsync();
+
+    }
+}
