5ff22103a9032fbd4bf00e6e431e86b1f93e12e0
Pablo_Gamboa <juan.gamboavargas@ucr.ac.cr>
2020-11-15 11:55:59 -0600

    Removing models and tables that are not used in the checklist module.

--- PRIME@UCR.Infrastructure/DataProviders/Implementations/ApplicationDbContext.cs
+++ PRIME@UCR.Infrastructure/DataProviders/Implementations/ApplicationDbContext.cs
@@ -29,8 +29,6 @@ namespace PRIME_UCR.Infrastructure.DataProviders.Implementations
         public DbSet<Item> Item { get; set; }
         public DbSet<InstanceChecklist> InstanceChecklist { get; set; }
         public DbSet<InstanciaItem> InstanciaItem { get; set; }
-        public DbSet<InstanciaItemPadre> InstanciaItemPadres { get; set; }
-        public DbSet<InstanciaItemHoja> InstanciaItemHojas { get; set; }
         public DbSet<Provincia> Provinces { get; set; }
         public DbSet<Pais> Countries { get; set; }
         public DbSet<Domicilio> HouseholdLocations { get; set; }
@@ -82,8 +80,6 @@ namespace PRIME_UCR.Infrastructure.DataProviders.Implementations
             builder.ApplyConfiguration(new TipoListaChequeoMap());
             builder.ApplyConfiguration(new InstanceChecklistMap());
             builder.ApplyConfiguration(new InstanciaItemMap());
-            builder.ApplyConfiguration(new InstanciaItemPadreMap());
-            builder.ApplyConfiguration(new InstanciaItemHojaMap());
             builder.ApplyConfiguration(new PaisMap());
             builder.ApplyConfiguration(new ProvinciaMap());
             builder.ApplyConfiguration(new DistritoMap());
