1b3cbeee47df450326b5a5829f7d985eb91b105c
Ricardo Franco <richifran1301@gmail.com>
2020-11-16 15:10:43 -0600

    Worked on GetAllTransportUnitsByMode integration tests on Assignement Service

---
+++ PRIME@UCR.Test/IntegrationTests/Incidents/AssignementServiceIntegrationTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PRIME_UCR.Application.Services.Incidents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PRIME_UCR.Test.IntegrationTests.Incidents
+{
+    public class AssignementServiceIntegrationTest: IClassFixture<IntegrationTestWebApplicationFactory<Startup>>
+    {
+        private readonly IntegrationTestWebApplicationFactory<Startup> _factory;
+
+        public AssignementServiceIntegrationTest(IntegrationTestWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAllTransportUnitsByModeReturnsEmpty()
+        {
+
+            var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
+            var result = await assignementService.GetAllTransportUnitsByMode(null);
+            Assert.Empty(result);
+        } 
+
+        [Fact]
+        public async Task GetAllTransportUnitsByModeReturnsValid()
+        {
+
+            var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
+            var result = await assignementService.GetAllTransportUnitsByMode("Terrestre");
+            Assert.Collection(result,
+                                  unidad => Assert.Equal("BPC086", unidad.Matricula),
+                                  unidad => Assert.Equal("FMM420", unidad.Matricula)
+                                  );
+        } 
+
+        [Fact]
+        public async Task GetAllTransportUnitsByModeReturnsInvalid()
+        {
+
+            var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
+            var result = await assignementService.GetAllTransportUnitsByMode("Invalid unit");
+            Assert.Null(result);
+        } 
+
+    }
+}
