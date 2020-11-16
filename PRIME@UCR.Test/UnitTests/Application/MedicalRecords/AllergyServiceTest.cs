5f7fc39c63d08189061c54d8567a3fe543ede406
Adrian Cordoba Soto <adrian.cordobasoto@ucr.ac.cr>
2020-11-15 20:55:49 -0600

    Addinf test to AllergyService

---
+++ PRIME@UCR.Test/UnitTests/Application/MedicalRecords/AllergyServiceTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using PRIME_UCR.Domain.Models.UserAdministration;
+using System.Reflection;
+using System.Threading.Tasks;
+using PRIME_UCR.Application.Repositories.MedicalRecords;
+using PRIME_UCR.Domain.Models.MedicalRecords;
+using PRIME_UCR.Application.Implementations.MedicalRecords;
+using PRIME_UCR.Infrastructure.Repositories.Sql.MedicalRecords;
+using System.Linq;
+
+namespace PRIME_UCR.Test.UnitTests.Application.MedicalRecords
+{
+    public class AllergyServiceTest
+    {
+
+        [Fact]
+        public async void getAllergyByRecordIdEmpty()
+        {
+            var mockRepo = new Mock<IAlergyRepository>();
+            var mockRepoList = new Mock<IAlergyListRepository>();
+            mockRepo.Setup(p => p.GetByConditionAsync(a => a.IdExpediente == -1)).Returns(Task.FromResult<IEnumerable<Alergias>>(null));
+            //mockRepoList.Setup()
+            //var store = new Mock<IUserStore<Usuario>>();
+            //var mockUserManager = new Mock<UserManager<Usuario>>(store.Object, null, null, null, null, null, null, null, null);
+            var AllergyService = new AlergyService(mockRepo.Object,mockRepoList.Object);
+            var result = await AllergyService.GetAlergyByRecordId(-1);
+            var EmptyAllergyList = new List<Alergias>();
+            Assert.Equal(result,EmptyAllergyList);
+        }
+
+        [Fact]
+        public async void getAllergyByRecordIdReturnsValidAllergy()
+        {
+            var mockRepo = new Mock<IAlergyRepository>();
+            var mockRepoList = new Mock<IAlergyListRepository>();
+            var AllergyService = new AlergyService(mockRepo.Object, mockRepoList.Object);
+            var ExpectedIdList = 1;
+            var ExpectedIdRecord = 1;
+            var result = (await AllergyService.GetAlergyByRecordId(1)).ToList();
+            Assert.Equal(ExpectedIdList, result[0].IdListaAlergia);
+            Assert.Equal(ExpectedIdRecord, result[0].IdExpediente);
+        }
+
+        /*
+       [Fact]
+       public async void getUsuarioWithDetailsReturnsValidUser()
+       {
+           var mockRepo = new Mock<IUsuarioRepository>();
+           mockRepo
+               .Setup(p => p.GetWithDetailsAsync("a6f7aa70-a038-419f-9945-7c77b093d58f"))
+               .Returns(Task.FromResult<Usuario>(new Usuario
+               {
+                   Id = "a6f7aa70-a038-419f-9945-7c77b093d58f",
+                   UserName = "juan.guzman@prime.com",
+                   NormalizedUserName = "JUAN.GUZMAN@PRIME.COM",
+                   Email = "juan.guzman@prime.com",
+                   NormalizedEmail = "JUAN.GUZMAN@PRIME.COM",
+                   EmailConfirmed = false,
+                   PasswordHash = "AQAAAAEAACcQAAAAEKBfjZVSMkEvJ3kJikd/FETuy1hxI3csK3qM2EwHBlQpgixfBX3tUaxpposHbUfakg==",
+                   SecurityStamp = "M7SUOG4MXMPBKLX2BN34HVOG7GRGNIDQ",
+                   ConcurrencyStamp = "8caf2844-e5ad-452c-b89f-016d71b5d09e",
+                   PhoneNumber = null,
+                   PhoneNumberConfirmed = false,
+                   TwoFactorEnabled = false,
+                   LockoutEnd = null,
+                   LockoutEnabled = true,
+                   AccessFailedCount = 0
+               })) ;
+           var store = new Mock<IUserStore<Usuario>>();
+           var mockUserManager = new Mock<UserManager<Usuario>>(store.Object, null, null, null, null, null, null, null, null);
+           var userService = new UsersService(mockRepo.Object, mockUserManager.Object);
+           var result = await userService.getUsuarioWithDetails("a6f7aa70-a038-419f-9945-7c77b093d58f");
+           Assert.Equal("a6f7aa70-a038-419f-9945-7c77b093d58f" , result.Id);
+           Assert.Equal("juan.guzman@prime.com", result.Email);
+           Assert.Equal("AQAAAAEAACcQAAAAEKBfjZVSMkEvJ3kJikd/FETuy1hxI3csK3qM2EwHBlQpgixfBX3tUaxpposHbUfakg==" , result.PasswordHash);
+           Assert.Equal("M7SUOG4MXMPBKLX2BN34HVOG7GRGNIDQ" , result.SecurityStamp);
+       }
+       */
+    }
+}
