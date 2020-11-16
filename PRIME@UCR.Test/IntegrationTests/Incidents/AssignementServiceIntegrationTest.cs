0560e6025f9edc08e1a101cbd6ed6d407b16ad3b
ADRIAN.SIBAJARETANA <adrian.sibajaretana@ucr.acr.cr>
2020-11-16 17:27:52 -0600

    Integration tests for Assignment service added.
    - GetCoordinatorsAsyncReturnsValid
    - GetAssignedOriginDoctorReturnsNull
    - GetAssignedDestinationDoctorReturnsNull
    
    TO DO: Merge with bugfix branch

--- PRIME@UCR.Test/IntegrationTests/Incidents/AssignementServiceIntegrationTest.cs
+++ PRIME@UCR.Test/IntegrationTests/Incidents/AssignementServiceIntegrationTest.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace PRIME_UCR.Test.IntegrationTests.Incidents
 {
-    public class AssignementServiceIntegrationTest: IClassFixture<IntegrationTestWebApplicationFactory<Startup>>
+    public class AssignementServiceIntegrationTest : IClassFixture<IntegrationTestWebApplicationFactory<Startup>>
     {
         private readonly IntegrationTestWebApplicationFactory<Startup> _factory;
 
@@ -24,7 +24,7 @@ namespace PRIME_UCR.Test.IntegrationTests.Incidents
             var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
             var result = await assignementService.GetAllTransportUnitsByMode(null);
             Assert.Empty(result);
-        } 
+        }
 
         [Fact]
         public async Task GetAllTransportUnitsByModeReturnsValid()
@@ -36,7 +36,7 @@ namespace PRIME_UCR.Test.IntegrationTests.Incidents
                                   unidad => Assert.Equal("BPC086", unidad.Matricula),
                                   unidad => Assert.Equal("FMM420", unidad.Matricula)
                                   );
-        } 
+        }
 
         [Fact]
         public async Task GetAllTransportUnitsByModeReturnsInvalid()
@@ -45,7 +45,49 @@ namespace PRIME_UCR.Test.IntegrationTests.Incidents
             var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
             var result = await assignementService.GetAllTransportUnitsByMode("Invalid unit");
             Assert.Null(result);
-        } 
+        }
+
+        [Fact]
+        public async Task GetCoordinatorsAsyncReturnsValid()
+        {
+            /* Case: The service should return the exact same number of Medical Coordinators 
+             *  that was written on the post deployment script
+             * -> Returns: A list with the coordinators found by the repository. 
+            */
+            int numberOfCordinatorsinDB = 2;
+            var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
+            var result = await assignementService.GetCoordinatorsAsync();
+            var list = result.ToList();
+            Assert.Equal(numberOfCordinatorsinDB, list.Count);
+        }
+
+        [Fact]
+        public async Task GetAssignedOriginDoctorReturnsNull()
+        {
+            /* Case: The service should return the assigned doctor in the origin of the incident. 
+             *  in this case, the incident's origin is a household thus it does not have an 
+             *  origin doctor assigned.
+             * -> Returns: Null
+            */
+            var incidentCode = "2020-11-16-0001-IT-TER";
+            var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
+            var result = await assignementService.GetAssignedOriginDoctor(incidentCode);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetAssignedDestinationDoctorReturnsNull()
+        {
+            /* Case: The service should return the assigned doctor in the destination of the incident. 
+             *  in this case, the destination has not been assigned yet. Thus the test should return 
+             *  null. 
+             * -> Returns: Null
+            */
+            var incidentCode = "2020-11-16-0001-IT-TER";
+            var assignementService = _factory.Services.GetRequiredService<IAssignmentService>();
+            var result = await assignementService.GetAssignedDestinationDoctor(incidentCode);
+            Assert.Null(result);
+        }
 
     }
 }
