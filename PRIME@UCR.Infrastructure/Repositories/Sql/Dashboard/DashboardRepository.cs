c8dbb64347c1170f1bfdc24761b657979832bb54
Jose Viquez Ramirez <jose.viquezramirez@ucr.ac.cr>
2020-11-16 10:07:33 -0600

    Concurrency error when loading incidents fixed.
    UserStory:      PIG01IIC20-567  ATE - Ver gráfico de incidentes en el tiempo
                    PIG01IIC20-520  ATE - Gráfico incidentes por cada medio de transporte
                    PIG01IIC20-568  ATE - Gráfico de casos atendidos por ubicación
    
    Changes:
    - Updates on dashboard repository to use RepoDB to solve concurrency error.

--- PRIME@UCR.Infrastructure/Repositories/Sql/Dashboard/DashboardRepository.cs
+++ PRIME@UCR.Infrastructure/Repositories/Sql/Dashboard/DashboardRepository.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using PRIME_UCR.Application.DTOs.Dashboard;
 using PRIME_UCR.Application.Repositories.Dashboard;
 using PRIME_UCR.Application.Services.UserAdministration;
 using PRIME_UCR.Domain.Models;
 using PRIME_UCR.Infrastructure.DataProviders;
 using PRIME_UCR.Infrastructure.Permissions.Dashboard;
+using RepoDb;
+using RepoDb.Extensions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,10 +64,28 @@ namespace PRIME_UCR.Infrastructure.Repositories.Sql.Dashboard
          */
         public async Task<List<Incidente>> GetAllIncidentsAsync()
         {
-            return await _db.Incidents
-                .Include(i => i.Cita)
-                .AsNoTracking()
-                .ToListAsync();
+            var incidents = new List<Incidente>();
+            using (var connection = new SqlConnection(_db.ConnectionString))
+            {
+                var extractor = await connection.ExecuteQueryMultipleAsync(@"
+                    SELECT *
+                    FROM Incidente;
+
+                    SELECT *
+                    FROM Cita;
+                ");
+
+                incidents = extractor.Extract<Incidente>().AsList();
+                var appointments = extractor.Extract<Cita>().AsList();
+
+                foreach(var incident in incidents)
+                {
+                    incident.Cita = appointments.FirstOrDefault(a => a.Id == incident.CodigoCita);
+                }
+
+            }
+            return incidents;
+
         }
 
         /**
@@ -73,7 +95,12 @@ namespace PRIME_UCR.Infrastructure.Repositories.Sql.Dashboard
          */
         public async Task<List<Distrito>> GetAllDistrictsAsync()
         {
-            return await _db.Districts.AsNoTracking().ToListAsync();
+            var result = new List<Distrito>();
+            using (var connection = new SqlConnection(_db.ConnectionString))
+            {
+                result = (await connection.QueryAllAsync<Distrito>()).AsList();
+            }
+            return result;
         }
 
         public Task<Incidente> GetByKeyAsync(string key)
