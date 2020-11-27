05e1e5fe81c25fecc5b8ccea6156e3bf56b58d53
daniela.vargassauma <daniela.vargassauma@ucr.ac.cr>
2020-11-26 18:12:53 -0600

    Created the change password functionality
    UserStory: PIG01IIC20-158
    ATE - Cambiar contraseña
    
    Changes:
    - Created the "PasswordChange" component
    - Created the "PasswordChange" page
    - Creted the DTO "ChangePasswordModel"
    
    Developer:
    -Daniela Vargas

---
+++ PRIME@UCR/Pages/UserAdministration/PasswordChange.razor.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using PRIME_UCR.Application.DTOs.UserAdministration;
+using PRIME_UCR.Application.Services.UserAdministration;
+using PRIME_UCR.Domain.Models.UserAdministration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Pages.UserAdministration
+{
+    public partial class PasswordChange
+    {
+        [Parameter]
+        public string OldPassword { get; set; }
+
+        [Parameter]
+        public string NewPassword { get; set; }
+
+        [Parameter]
+        public string NewPasswordConfirm { get; set; }
+
+        [Inject]
+        public UserManager<Usuario> UserManager { get; set; }
+
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationState { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public ChangePasswordModel ChangePasswordModel;
+
+        public bool isBusy;
+        
+        public char ValidationState = 'N';
+
+        public char ResultOfRecovery = 'N';
+
+        public char NewPasswordConfirmed = 'N';
+
+        public char OldPasswordConfirmed = 'N';
+
+
+
+        protected override async Task OnParametersSetAsync()
+        {
+            ChangePasswordModel = new ChangePasswordModel();
+            ChangePasswordModel.OldPassword = OldPassword;
+            ChangePasswordModel.NewPassword = NewPassword;
+            ChangePasswordModel.NewPasswordConfirm = NewPasswordConfirm;
+            await ValidateUser();
+        }
+
+        public async Task ValidateUser()
+        {
+            var emailUser = (await authenticationState).User.Identity.Name;
+            var user = await UserManager.FindByEmailAsync(emailUser);
+
+            if (user != null )
+            {
+                ValidationState = 'T';
+            }
+            else
+            {
+                ValidationState = 'F';
+            }
+        }
+
+
+        public async Task ChangePassword()
+        {
+            isBusy = true;
+
+            var emailUser = (await authenticationState).User.Identity.Name;
+
+            var user = await UserManager.FindByEmailAsync(emailUser);
+
+            if (user != null)
+            {
+                var correctPass = await UserManager.CheckPasswordAsync(user, ChangePasswordModel.OldPassword);
+                if (correctPass == false)
+                {
+                    OldPasswordConfirmed = 'F';
+                }
+                else
+                {
+                    OldPasswordConfirmed = 'T';
+                    if (ChangePasswordModel.NewPassword != ChangePasswordModel.NewPasswordConfirm)
+                    {
+                        NewPasswordConfirmed = 'F';
+                    }
+                    else
+                    {
+                        NewPasswordConfirmed = 'V';
+                        var result = await UserManager.ChangePasswordAsync(user, ChangePasswordModel.OldPassword, ChangePasswordModel.NewPassword);
+                        ResultOfRecovery = result.Succeeded ? 'T' : 'F';
+                    }
+                }
+                    
+            }
+            else
+            {
+                ResultOfRecovery = 'F';
+            }
+            isBusy = false;
+            StateHasChanged();
+            if (ResultOfRecovery != 'T')
+            {
+                return;
+            }
+
+        }
+    }
+}
