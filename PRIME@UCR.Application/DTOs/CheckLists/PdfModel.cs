674a122b29dc390d6999da8744375a869006701f
Pablo_Gamboa <juan.gamboavargas@ucr.ac.cr>
2020-11-29 16:16:46 -0600

    Completing the Pdf component. Story: PIG01IIC20-696 Changes: - Adding parameters and services to the component.

--- PRIME@UCR.Application/DTOs/CheckLists/PdfModel.cs
+++ PRIME@UCR.Application/DTOs/CheckLists/PdfModel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using PRIME_UCR.Application.Dtos.Incidents;
+using PRIME_UCR.Domain.Models.MedicalRecords;
 
 namespace PRIME_UCR.Application.DTOs.CheckLists
 {
     public class PdfModel
     {
         public IncidentDetailsModel Incident { get; set; }
+        public IEnumerable<PadecimientosCronicos> ChronicConditions { get; set; }
     }
 }
--- PRIME@UCR/Components/CheckLists/PdfComponent.razor.cs
+++ PRIME@UCR/Components/CheckLists/PdfComponent.razor.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using PRIME_UCR.Application.DTOs.CheckLists;
+using PRIME_UCR.Application.DTOs.Incidents;
 using PRIME_UCR.Application.Services.CheckLists;
+using PRIME_UCR.Application.Services.Incidents;
+using PRIME_UCR.Application.Services.MedicalRecords;
 using Microsoft.JSInterop;
 
 namespace PRIME_UCR.Components.CheckLists
@@ -13,12 +17,26 @@ namespace PRIME_UCR.Components.CheckLists
     {
         [Inject] private Microsoft.JSInterop.IJSRuntime JS { get; set; }
         [Inject] private IPdfService MyPdfService { get; set; }
+        [Inject] private IIncidentService MyIncidentService { get; set; }
+        [Inject] private IChronicConditionService MyChronicConditionService { get; set; }
+        // Pasar otro parametro que sea lo que incidentes utiliza en el listado de todos los incidentes.
+        [Parameter] public string IncidentCode { get; set; }
 
+        private bool Disabled { get; set; } = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
         protected async Task ExportToPdf()
         {
+            PdfModel pdfModel = new PdfModel();
+            pdfModel.Incident = await MyIncidentService.GetIncidentDetailsAsync(IncidentCode);
+            pdfModel.ChronicConditions = await MyChronicConditionService.GetChronicConditionByRecordId(pdfModel.Incident.MedicalRecord.Id);
+            string fileName = "Reporte " + IncidentCode + ".pdf";
             using (MemoryStream excelStream = MyPdfService.GenerateIncidentPdf())
             {
-                await JS.InvokeAsync<object>("saveAsFile", "Reporte.pdf", Convert.ToBase64String(excelStream.ToArray()));
+                await JS.InvokeAsync<object>("saveAsFile", fileName, Convert.ToBase64String(excelStream.ToArray()));
             }
         }
     }
