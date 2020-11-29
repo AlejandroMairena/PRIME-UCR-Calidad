49f1f2d30b247ea60250df57507eff904ab6b9de
jaszdev <javier.sandoval@ucr.ac.cr>
2020-11-29 14:01:03 -0600

    Improvements of video recording interface.
    PIG01IIC20-687

--- PRIME@UCR/Components/Multimedia/VideoComponent.razor.cs
+++ PRIME@UCR/Components/Multimedia/VideoComponent.razor.cs
@@ -20,7 +20,7 @@ namespace PRIME_UCR.Components.Multimedia
         ElementReference recordedVideo;
         ElementReference downloadButton;
         ElementReference stopButton;
-
+        ElementReference closeButton;
 
         protected override void OnInitialized()
         {
@@ -32,13 +32,42 @@ namespace PRIME_UCR.Components.Multimedia
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             // call JS code to initialize Element References
-            //await JS.InvokeAsync<bool>("videoInit", startButton, videoPreview, recordedVideo, downloadButton, stopButton);
+            await JS.InvokeAsync<bool>("videoInit", startButton, videoPreview, recordedVideo, downloadButton, stopButton, closeButton);
         }
 
         async Task CloseComponent()
         {
             if (MultimediaModal != null)
                 MultimediaModal.OnModalClosed -= CloseComponent;
+        }
+
+        // Open/Close Button
+        bool cameraOpen = false;
+        bool cameraClose => !cameraOpen;
+        string openCloseButtonText => !cameraOpen ? "Abrir Cámara" : "Cerrar Cámara";
+
+        async Task HandleOpenCloseClick()
+        {
+            if (!cameraOpen)
+                await JS.InvokeAsync<bool>("openCamera", videoPreview);
+            else
+            {
+                await JS.InvokeAsync<bool>("stop", videoPreview, true);
+                await JS.InvokeAsync<bool>("closeCamera", videoPreview);
+            }
+            cameraOpen = !cameraOpen;
+        }
+
+        async Task OnClose()
+        {
+            await JS.InvokeAsync<bool>("stop", videoPreview, true);
+            MultimediaModal?.CloseImageView();
+        }
+
+        async void OnTitleChanged(Tuple<bool, string> tuple)
+        {
+            if (!tuple.Item1) // if valid file name
+                await JS.InvokeAsync<bool>("setDownloadName", tuple.Item2);
         }
 
     }
