870b2d4288ebfca6895cebc57470f417cb74e37f
Jose Viquez Ramirez <jose.viquezramirez@ucr.ac.cr>
2020-11-29 22:32:53 -0600

    Initial implementation of UI to handle individual filter remover
    UserStory:	PIG01IIC20-661	ATE - Remover filtros individualmente
    
    Changes:
    - Added generic individual filter remover.
    - Added component that handles all individual filters' remover.
    - Updates on dashboard page to use the individual filters' remover.

---
+++ PRIME@UCR/Components/Dashboard/Filters/GenericIndividualFilterRemover.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.Dashboard.Filters
+{
+    public partial class GenericIndividualFilterRemover
+    {
+        [Parameter]
+        public string Label { get; set; }
+    }
+}
---
+++ PRIME@UCR/Components/Dashboard/IndividualFiltersRemoverComponent.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using PRIME_UCR.Application.DTOs.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.Dashboard
+{
+    public partial class IndividualFiltersRemoverComponent
+    {
+        [Parameter]
+        public FilterModel FilterInfo { get; set; }
+
+        [Parameter]
+        public EventCallback<FilterModel> FilterInfoChanged { get; set; }
+
+        private void Test()
+        {
+
+        }
+    }
+}
