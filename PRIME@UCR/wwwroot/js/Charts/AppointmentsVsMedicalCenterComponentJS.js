acd4214790fb2bd1ca7b4e1082c080cbd4d1a0f9
Elian Ortega <elian.mortega@gmail.com>
2020-12-03 01:15:00 -0600

    - Change appointments vs medical center from bar graph to pie graph     User Story:     - Appointments Vs medical center Graph
        Changes:
        - Change form bar to pie graph

--- PRIME@UCR/wwwroot/js/Charts/AppointmentsVsMedicalCenterComponentJS.js
+++ PRIME@UCR/wwwroot/js/Charts/AppointmentsVsMedicalCenterComponentJS.js
@@ -28,54 +28,25 @@ function CreateAppointmentsVsMedicalCenterComponentJS(results) {
         am4core.useTheme(am4themes_animated);
         // Themes end
 
-        var chart = am4core.create("AppointmentsVsMedicalCenterComponent", am4charts.XYChart);
+        var chart = am4core.create("AppointmentsVsMedicalCenterComponent", am4charts.PieChart);
         chart.padding(40, 40, 40, 40);
 
         //Change language 
         chart.language.locale = am4lang_es_ES;
 
-        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.title.text = "Centro Medico";
-        categoryAxis.title.fontWeight = "bold";
-        categoryAxis.renderer.grid.template.location = 0;
-        categoryAxis.dataFields.category = "destination";
-        categoryAxis.renderer.minGridDistance = 1;
-        categoryAxis.renderer.inversed = true;
-        categoryAxis.renderer.grid.template.disabled = true;
-
-        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-        //Chart Scale
-        valueAxis.min = 0;
-        valueAxis.max = 50;
-
-        valueAxis.min = 0;
-        valueAxis.title.text = "Cantidad de Citas";
-        valueAxis.title.fontWeight = "bold";
-
-
-        var series = chart.series.push(new am4charts.ColumnSeries());
-        series.dataFields.categoryY = "destination";
-        series.dataFields.valueX = "quantity";
-        //series.tooltipText = "{valueX.value}"
-        series.columns.template.strokeOpacity = 0;
-        series.columns.template.column.cornerRadiusBottomRight = 5;
-        series.columns.template.column.cornerRadiusTopRight = 5;
-        series.columns.template.tooltipText = "{categoryY}: [bold]{valueX}";
-
-
-        var labelBullet = series.bullets.push(new am4charts.LabelBullet())
-        labelBullet.label.horizontalCenter = "left";
-        labelBullet.label.dx = 10;
-        labelBullet.label.text = "{values.valueX.workingValue.formatNumber('#')}";
-        labelBullet.locationX = 1;
-
-        // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
-        series.columns.template.adapter.add("fill", function (fill, target) {
-            return chart.colors.getIndex(target.dataItem.index);
-        });
-
-        categoryAxis.sortBySeries = series;
         chart.data = chartData;
+
+        var pieSeries = chart.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "quantity";
+        pieSeries.dataFields.category = "destination";
+        pieSeries.slices.template.stroke = am4core.color("#fff");
+        pieSeries.slices.template.strokeWidth = 2;
+        pieSeries.slices.template.strokeOpacity = 1;
+
+        // This creates initial animation
+        pieSeries.hiddenState.properties.opacity = 1;
+        pieSeries.hiddenState.properties.endAngle = -90;
+        pieSeries.hiddenState.properties.startAngle = -90;
 
 
     }); // end am4core.ready()
