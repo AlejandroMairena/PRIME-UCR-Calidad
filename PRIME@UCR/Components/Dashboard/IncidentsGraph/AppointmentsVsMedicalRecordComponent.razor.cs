df5e3dee2bc261050f72950ec279a192be1ef5f2
Elian Ortega <elian.mortega@gmail.com>
2020-12-05 14:52:01 -0600

    - Created firt aproach for medical changes overtime graphs

---
+++ PRIME@UCR/Components/Dashboard/IncidentsGraph/AppointmentsVsMedicalRecordComponent.razor.cs
@@ -0,0 +1,112 @@
+﻿using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using PRIME_UCR.Application.DTOs.Dashboard;
+using PRIME_UCR.Application.Services.Dashboard;
+using PRIME_UCR.Application.Services.Incidents;
+using PRIME_UCR.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRIME_UCR.Components.Dashboard.IncidentsGraph
+{
+    public partial class AppointmentsVsMedicalRecordComponent
+    {
+        [Parameter] public DashboardDataModel Data { get; set; }
+        [Parameter] public EventCallback<DashboardDataModel> DataChanged { get; set; }
+        [Parameter] public EventCallback OnDiscard { get; set; }
+
+        [Parameter] public bool ZoomActive { get; set; }
+
+
+        private int appointmentsQuantity { get; set; }
+
+        [Inject]
+        IJSRuntime JS { get; set; }
+
+        [Inject]
+        IModalService Modal { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GenerateAppointmentsVsMedicalCenterComponent();
+        }
+
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await GenerateAppointmentsVsMedicalCenterComponent();
+        }
+
+        /*
+         *  GenerateAppointmentsVsMedicalCenterComponent()
+         *  
+         *  Method that generates the IncidentsVsDestination Graph 
+         *  based on the incoming list from the dashboard service in which 
+         *  data is already filtered 
+         *  
+         */
+        private async Task GenerateAppointmentsVsMedicalCenterComponent()
+        {
+            //TODO: Use Cita Data to create this
+            //var incidentsData = Data.filteredIncidentsData;
+
+            //eventQuantity = incidentsData.Count();
+
+            var medicalCenters = Data.medicalCenters;
+
+            /*var incidentsPerDestination = incidentsData.GroupBy(i => {
+                if (i.Destino != null)
+                {
+                    var cu = i.Destino as CentroUbicacion;
+                    return cu.CentroMedicoId;
+                }
+                else
+                {
+                    return 0;
+                }
+            });*/
+
+            appointmentsQuantity = 11;
+            var results = new List<String>();
+            var appointments = new List<int>() { 2, 3, 3, 3, 3 };
+            var i = 0;
+            foreach (var medicalCenter in medicalCenters)
+            {
+                /*
+                var labelName = "No Asignado";
+                if (incidents.Key != 0)
+                {
+                    var medicalCenter = medicalCenters.Where((medCenter) => incidents.ToList().First().Destino is CentroUbicacion cu
+                                                                                        && cu.CentroMedicoId == medCenter.Id);
+                    if (medicalCenter.Any())
+                    {
+                        labelName = medicalCenter.First().Nombre;
+                    }
+                }*/
+                results.Add(medicalCenter.Nombre);
+                results.Add(appointments[i].ToString());
+                i++;
+                //results.Add(incidents.ToList().Count().ToString());
+            }
+
+            await JS.InvokeVoidAsync("CreateAppointmentsVsMedicalRecordsComponentWeightJS", (object)results);
+        }
+
+        void ShowModal()
+        {
+            var modalOptions = new ModalOptions()
+            {
+                Class = "graph-zoom-modal blazored-modal"
+            };
+
+            var parameters = new ModalParameters();
+            parameters.Add(nameof(AppointmentsVsMedicalCenterComponent.Data), Data);
+            parameters.Add(nameof(AppointmentsVsMedicalCenterComponent.ZoomActive), true);
+            Modal.Show<AppointmentsVsMedicalCenterComponent>("Evolucion Metricas de Paciente", parameters, modalOptions);
+        }
+    }
+}
---
+++ PRIME@UCR/wwwroot/js/Charts/PatientVsMedicalRecordsComponentJS.js
@@ -0,0 +1,115 @@
+﻿/**
+ * Funciton that creates the Appointment VS medical center component
+ * Params: result
+ * [results] only contains an array the the data to include in the graph
+ * 
+ * NO SENSITIVE DATA SHOULD BE TREATED IN THIS CODE
+ */
+
+function CreateAppointmentsVsMedicalRecordsComponentWeightJS(results) {
+    am4core.ready(function () {
+        /*
+
+        var chartData = [];
+
+        for (var i = 0; i < results.length; i += 2) {
+            var destination = results[i];
+            var quantity = results[i + 1];
+
+
+            chartData.push({
+                "destination": destination,
+                "quantity": quantity
+            });
+        }
+
+*/
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+        var chart = am4core.create("AppointmentsVsMedicalRecordsComponent", am4charts.XYChart);
+
+        // Add data
+        chart.data = [{
+            "date": new Date(2020, 3, 21),
+            "value": 60
+        }, {
+                "date": new Date(2020, 4, 22),
+            "value": 63
+        }, {
+                "date": new Date(2020, 5, 23),
+            "value": 62
+        }, {
+                "date": new Date(2020, 6, 24),
+            "value": 60
+        }, {
+                "date": new Date(2020, 7, 25),
+            "value": 61
+        }, {
+                "date": new Date(2020, 8, 26),
+            "value": 62,
+            "disabled": false
+        }];
+
+
+
+        // Create axes
+        var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+
+        dateAxis.title.text = "Fecha de la Cita";
+        dateAxis.title.align = "center";
+        dateAxis.title.fontWeight = 600;
+        dateAxis.renderer.grid.template.location = 0.5;
+        dateAxis.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+        dateAxis.renderer.minGridDistance = 40;
+        dateAxis.tooltipDateFormat = "MMM dd, yyyy";
+        dateAxis.dateFormats.setKey("day", "dd");
+        // Create value axis
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        valueAxis.title.text = "Peso del Paciente";
+        valueAxis.title.align = "center";
+        valueAxis.title.fontWeight = 600;
+
+        //Graph Scale
+        valueAxis.min = 0;
+        valueAxis.max = 100;
+        valueAxis.maxPrecision = 0;
+
+        // Create series
+        var lineSeries = chart.series.push(new am4charts.LineSeries());
+
+        lineSeries.dataFields.valueY = "value";
+        lineSeries.dataFields.dateX = "date";
+        lineSeries.name = "Sales";
+        lineSeries.strokeWidth = 3;
+        lineSeries.strokeDasharray = "5,4";
+
+        // Add simple bullet
+        var bullet = lineSeries.bullets.push(new am4charts.CircleBullet());
+        bullet.disabled = true;
+        bullet.propertyFields.disabled = "disabled";
+
+        var secondCircle = bullet.createChild(am4core.Circle);
+        secondCircle.radius = 6;
+        secondCircle.fill = chart.colors.getIndex(8);
+
+
+        bullet.events.on("inited", function (event) {
+            animateBullet(event.target.circle);
+        })
+
+
+        //Animate final bullet
+        function animateBullet(bullet) {
+            var animation = bullet.animate([{ property: "scale", from: 1, to: 5 }, { property: "opacity", from: 1, to: 0 }], 1000, am4core.ease.circleOut);
+            animation.events.on("animationended", function (event) {
+                animateBullet(event.target.object);
+            })
+        }
+
+
+    }); // end am4core.ready()
+}
